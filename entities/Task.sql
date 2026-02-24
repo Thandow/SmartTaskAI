0ab06a683b7f4b847b8aeec35773d1335e9a7fbe
Thando Mngomezulu <125863913+Thandow@users.noreply.github.com>
2026-02-24 14:51:52 +0200

    Update Task.sql

--- entities/Task.sql
+++ entities/Task.sql
@@ -32,4 +32,65 @@
     },
     "category": {
       "type": "string",
- 
+      "enum": [
+        "work",
+        "personal",
+        "study",
+        "health",
+        "meeting",
+        "errand",
+        "other"
+      ],
+      "default": "personal"
+    },
+    "due_date": {
+      "type": "string",
+      "format": "date-time"
+    },
+    "suggested_time": {
+      "type": "string"
+    },
+    "estimated_minutes": {
+      "type": "number"
+    },
+    "points": {
+      "type": "number",
+      "default": 10
+    },
+    "completed_date": {
+      "type": "string",
+      "format": "date-time"
+    },
+    "ai_notes": {
+      "type": "string"
+    },
+    "mood_tag": {
+      "type": "string",
+      "enum": [
+        "energized",
+        "normal",
+        "tired",
+        "overwhelmed"
+      ]
+    },
+    "assigned_to": {
+      "type": "string",
+      "description": "Email of the user this task is assigned to"
+    },
+    "assigned_to_name": {
+      "type": "string",
+      "description": "Display name of assigned user"
+    },
+    "project_id": {
+      "type": "string",
+      "description": "ID of the project this task belongs to"
+    },
+    "project_name": {
+      "type": "string",
+      "description": "Name of the project"
+    }
+  },
+  "required": [
+    "title"
+  ]
+}
